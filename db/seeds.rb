fcaf5df064f516b313072cd4df15522073223448
nicolas diaz almassio <nicolas.diaz.almassio@gmail.com>
2021-02-16 18:36:54 -0300

    Added user and message resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body:"Hola como estas", user_id:"1")
+Message.create(body:"Bien y vos?", user_id:"2")
+Message.create(body:"Hola a todos!", user_id:"3")
+Message.create(body:"Buen dia!!", user_id:"4")
+Message.create(body:"Que hermosa mañana", user_id:"5")
